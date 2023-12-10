40cb1ac23a296c389470c1fd28f4190edfeae5e3
Frederick Kurniawan <frederickkurniawan93@gmail.com>
2023-12-10 20:04:24 +0700

    Adding speed up power up

---
+++ Assets/Scripts/PowerUpManager.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUpManager : MonoBehaviour
+{
+    public Transform spawnArea;
+    public int maxPowerUpAmount;
+    public int spawnInterval;
+    public Vector2 powerUpAreaMin;
+    public Vector2 powerUpAreaMax;
+    public List<GameObject> powerUpTempelateList;
+
+    private List<GameObject> powerUpList;
+
+    private float timer;
+    public float despawnTime;
+
+    private void Start()
+    {
+        powerUpList = new List<GameObject>();
+        timer = 0;
+    }
+
+    private void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer > spawnInterval)
+        {
+            GenerateRandomPowerUp();
+            timer -= spawnInterval;
+        }
+    }
+    public void GenerateRandomPowerUp()
+    {
+        GenerateRandomPowerUp(new Vector2(Random.Range(powerUpAreaMin.x, powerUpAreaMax.x), Random.Range(powerUpAreaMin.y, powerUpAreaMax.y)));
+    }
+
+    public void GenerateRandomPowerUp(Vector2 position)
+    {
+        if (powerUpList.Count >= maxPowerUpAmount)
+        {
+            return;
+        }
+
+        if (position.x < powerUpAreaMin.x ||
+            position.x > powerUpAreaMax.x ||
+            position.y < powerUpAreaMin.y ||
+            position.y > powerUpAreaMax.y)
+        {
+            return;
+        }
+
+            int randomIndex = Random.Range(0, powerUpTempelateList.Count);
+
+            GameObject powerUp = Instantiate(powerUpTempelateList[randomIndex], new Vector3( position.x, position.y, powerUpTempelateList[randomIndex].transform.position.z), Quaternion.identity, spawnArea);
+            powerUp.SetActive(true);
+
+            powerUpList.Add(powerUp);
+            StartCoroutine(DespawnPowerUpAfterDelay(powerUp, despawnTime));
+    }
+
+    public void RemovePowerUp(GameObject powerUp)
+    {
+        powerUpList.Remove(powerUp);
+        Destroy(powerUp);
+    }
+
+    private IEnumerator DespawnPowerUpAfterDelay(GameObject powerUp, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        RemovePowerUp(powerUp);
+    }
+
+    public void RemoveAllPowerUp()
+    {
+        while(powerUpList.Count > 0)
+        {
+            RemovePowerUp(powerUpList[0]);
+        }
+    }
+}
